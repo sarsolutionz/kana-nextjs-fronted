a8abbcec02bef539a84966ef025d8ce11ef7e88c
satyaprakash-yadav <satyay9798@gmail.com>
2024-12-15 18:32:07 +0530

    fix: table props as a optional

--- src/features/members/components/filters-info.tsx
+++ src/features/members/components/filters-info.tsx
@@ -20,7 +20,7 @@ import { Input } from "@/components/ui/input";
 import { Loader } from "lucide-react";
 
 interface TableProps<TData> {
-  table: Table<TData>;
+  table?: Table<TData>;
 }
 
 export const FiltersInfo = <TData,>({ table }: TableProps<TData>) => {
